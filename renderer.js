3f2b424b03de86730eb5394fa132619a337140d2
darellanodev <darellano.aliaga@gmail.com>
2024-11-07 22:00:48 +0100

    closes #2 Set quality for videos

--- renderer.js
+++ renderer.js
@@ -5,9 +5,11 @@ const typeRadio = Array.from(document.getElementsByName('type'));
 const qualityRadio = Array.from(document.getElementsByName('quality'));
 
 const getQuality = (type) => {
+  const quality = qualityRadio.find(radio => radio.checked);
   if (type === 'audio') {
-    const quality = qualityRadio.find(radio => radio.checked);
     return quality.value === 'high' ? '--audio-quality 0' : '';
+  } else {
+    return quality.value === 'high' ? 'bestvideo+bestaudio[ext=m4a]' : 'best[ext=mp4]/best';
   }
 }
 saveButton.addEventListener('click', () => {
@@ -23,8 +25,10 @@ saveButton.addEventListener('click', () => {
     prefix = `yt-dlp -f bestaudio -x --audio-format mp3 ${quality} "`;
     suffix = '" -o "folder1/%%(title)s.%%(ext)s" --cookies-from-browser firefox --restrict-filenames';
   } else if (selectedType.value === 'video') {
+    const quality = getQuality(selectedType.value)
+
     prefix = 'yt-dlp.exe "https://www.youtube.com/watch?v=vrjCRv2vBqM" -f "';
-    suffix = '" -f "best[ext=mp4]/best" --cookies-from-browser firefox --restrict-filenames -o "folder1/%%(title)s.%%(ext)s"';
+    suffix = `" -f "${quality}" --cookies-from-browser firefox --restrict-filenames -o "folder1/%%(title)s.%%(ext)s"`;
   }
 
   const processedLines = lines.map(line => `${prefix} ${line} ${suffix}`);
