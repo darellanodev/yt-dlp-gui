2bb99ced1a56ee1d3abec3eb43d98ab5588ac41d
darellanodev <darellano.aliaga@gmail.com>
2024-11-12 22:36:27 +0100

    #3 Add a test for getting the quality parameter when the type is set to video and the quality is set to high

--- tests/ParamBuilder.test.js
+++ tests/ParamBuilder.test.js
@@ -5,3 +5,9 @@ test('getQuality when type is audio and quality is high', () => {
     const result = paramBuilder.getQuality('audio', 'high');
     expect(result).toBe('--audio-quality 0');
 });
+
+test('getQuality when type is video and quality is high', () => {
+    const paramBuilder = new ParamBuilder();
+    const result = paramBuilder.getQuality('video', 'high');
+    expect(result).toBe('bestvideo+bestaudio[ext=m4a]');
+})
