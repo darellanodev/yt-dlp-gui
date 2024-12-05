ce6bf155b7ef2dd6f773e8719c565d80799c97d3
darellanodev <darellano.aliaga@gmail.com>
2024-12-05 18:55:29 +0100

    Add the old html into the App.tsx (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,36 @@ function App() {
 
   return (
     <>
-      <div>in construction</div>
+      <h4>Configure the parameters</h4>
+      <div style={{ display: 'flex' }}>
+        <fieldset style={{ display: 'inline-block' }}>
+          <legend>Media type</legend>
+          <input type="radio" id="video" name="type" value="video" defaultChecked />
+          <label htmlFor="video">Video</label>
+          <br />
+          <input type="radio" id="audio" name="type" value="audio" />
+          <label htmlFor="audio">Audio</label>
+        </fieldset>
+        <fieldset style={{ display: 'inline-block' }}>
+          <legend>Quality</legend>
+          <input type="radio" id="normal" name="quality" value="normal" defaultChecked />
+          <label htmlFor="normal">Normal</label>
+          <br />
+          <input type="radio" id="high" name="quality" value="high" />
+          <label htmlFor="high">High</label>
+        </fieldset>
+        <fieldset style={{ display: 'inline-block' }}>
+          <legend>Output folder (optional)</legend>
+          <input type="text" id="folder" name="folder" placeholder="insert folder" />
+        </fieldset>
+      </div>
+
+      <h4>Insert the URLs</h4>
+      <textarea id="myTextarea" rows={5} cols={50} />
+      <button id="saveButton">Process</button>
+      <br />
+      <h4>Result of the commands to execute in yt-dlp</h4>
+      <textarea id="Results" rows={5} cols={50} />
     </>
   )
 }
