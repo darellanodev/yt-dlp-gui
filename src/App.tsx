d65e2deed66bc683ca149e0655db8577f5ca1c3a
darellanodev <darellano.aliaga@gmail.com>
2024-12-09 17:17:41 +0100

    Apply some Tailwind CSS styles (closes #12)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { ParamBuilder } from './utils/ParamBuilder';
 import { StringUtils } from './utils/StringUtils';
 import { UIManager } from './utils/UIManager';
@@ -25,38 +24,38 @@ function App() {
   }
 
   return (
-    <>
-      <h4>Configure the parameters</h4>
-      <div style={{ display: 'flex' }}>
-        <fieldset style={{ display: 'inline-block' }}>
-          <legend>Media type</legend>
-          <input type="radio" id="video" name="type" value="video" defaultChecked />
-          <label htmlFor="video">Video</label>
-          <br />
-          <input type="radio" id="audio" name="type" value="audio" />
-          <label htmlFor="audio">Audio</label>
-        </fieldset>
-        <fieldset style={{ display: 'inline-block' }}>
-          <legend>Quality</legend>
-          <input type="radio" id="normal" name="quality" value="normal" defaultChecked />
-          <label htmlFor="normal">Normal</label>
-          <br />
-          <input type="radio" id="high" name="quality" value="high" />
-          <label htmlFor="high">High</label>
-        </fieldset>
-        <fieldset style={{ display: 'inline-block' }}>
-          <legend>Output folder (optional)</legend>
-          <input type="text" id="folder" name="folder" placeholder="insert folder" />
-        </fieldset>
-      </div>
-
-      <h4>Insert the URLs</h4>
-      <textarea id="myTextarea" rows={5} cols={50} />
-      <button id="saveButton" onClick={handleClick}>Process</button>
+<div className="p-4 bg-gray-900 text-white">
+  <h4 className="text-lg font-bold mb-4">Configure the parameters</h4>
+  <div className="flex">
+    <fieldset className="bg-yellow-500 w-1/3 mr-4 p-4">
+      <legend className="font-bold bg-yellow-500 pl-2 pr-2">Media type</legend>
+      <input type="radio" id="video" name="type" value="video" defaultChecked />
+      <label htmlFor="video" className="ml-2">Video</label>
       <br />
-      <h4>Result of the commands to execute in yt-dlp</h4>
-      <textarea id="Results" rows={5} cols={50} />
-    </>
+      <input type="radio" id="audio" name="type" value="audio" />
+      <label htmlFor="audio" className="ml-2">Audio</label>
+    </fieldset>
+    <fieldset className="bg-blue-500 w-1/3 mr-4 p-4">
+      <legend className="font-bold bg-blue-500 pl-2 pr-2">Quality</legend>
+      <input type="radio" id="normal" name="quality" value="normal" defaultChecked />
+      <label htmlFor="normal" className="ml-2">Normal</label>
+      <br />
+      <input type="radio" id="high" name="quality" value="high" />
+      <label htmlFor="high" className="ml-2">High</label>
+    </fieldset>
+    <fieldset className="bg-green-500 w-1/3 p-4">
+      <legend className="font-bold bg-green-500 pl-2 pr-2">Output folder (optional)</legend>
+      <input type="text" id="folder" name="folder" placeholder="insert folder" className="w-full p-2 bg-green-900" />
+    </fieldset>
+  </div>
+
+  <h4 className="text-lg font-bold mt-8 mb-4">Insert the URLs</h4>
+  <textarea id="myTextarea" rows={5} cols={50} className="w-full p-4 bg-blue-900" />
+  <button id="saveButton" onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Process</button>
+  <br />
+  <h4 className="text-lg font-bold mt-8 mb-4">Result of the commands to execute in yt-dlp</h4>
+  <textarea id="Results" rows={5} cols={50} className="w-full p-4 bg-blue-900" />
+</div>
   )
 }
 
